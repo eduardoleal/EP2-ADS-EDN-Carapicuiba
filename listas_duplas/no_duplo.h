5414c303e06277f956b647aa19ba9ba0a96ce437
Eduardo Leal <lealeduardo@outlook.com>
2014-05-20 11:31:24 -0300

    renomeando funções

--- listas_duplas/no_duplo.h
+++
@@ -1,9 +0,0 @@
-typedef struct no_duplo * t_apont_no_duplo;
-
-typedef struct no_duplo {
-	int info;
-	t_apont_no_duplo ant;
-	t_apont_no_duplo prox;
-} t_no_duplo;
-
-t_apont_no_duplo cria_no_duplo (int);
